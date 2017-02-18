c8e1e8cf072c34dd2066005e7f3d2dc1afd06f36
Rostradamus <hyungro@hotmail.com>
2017-02-17 16:26:26 -0800

    added tests

---
+++ app/src/test/java/rostradamus/simplematrixcalculator/MatrixTest.java
@@ -0,0 +1,18 @@
+
+package rostradamus.simplematrixcalculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by rolee on 2017-02-17.
+ */
+
+public class MatrixTest {
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
